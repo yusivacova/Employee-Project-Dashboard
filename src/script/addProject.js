e1ee4dc8db8eec083c489631488885e70d120a38
yusivacova <sivacovayulia@gmail.com>
2026-04-26 23:36:16 +0300

    feat: add requirement for new project form

--- src/script/addProject.js
+++ src/script/addProject.js
@@ -1,10 +1,14 @@
 import { addRequirementForForm } from './requirementForms';
+import { addRequirementForFormBudget } from './requirementForms';
+import { addRequirementForFormEmployeeCapacity } from './requirementForms';
 
 export function addProject() {
   openFormAddProject();
   closeFormAddProject();
   addRequirementForFormProjectName();
   addRequirementForFormProjectCompany();
+  addRequirementForFormProjectBudget();
+  addRequirementForFormProjectEmployeeCapacity();
 }
 
 const openFormAddProject = () => {
@@ -23,6 +27,7 @@ const closeFormAddProject = () => {
     const clickTag = e.target;
     if (clickTag.className.includes('form-btn-cancel')) {
       formProjects.classList.remove('form-open');
+      removeFormInputAndNotification();
     }
   });
 };
@@ -35,4 +40,28 @@ const addRequirementForFormProjectName = () => {
 const addRequirementForFormProjectCompany = () => {
   const inputFormProjectsCompany = document.getElementById('form-projects__company');
   addRequirementForForm(inputFormProjectsCompany, 2);
+};
+
+const addRequirementForFormProjectBudget = () => {
+  const inputFormProjectsBudget = document.getElementById('form-projects__budget');
+  addRequirementForFormBudget(inputFormProjectsBudget);
+};
+
+const addRequirementForFormProjectEmployeeCapacity = () => {
+  const inputFormProjectsEmployeeCapacity = document.getElementById('form-projects__employee');
+  addRequirementForFormEmployeeCapacity(inputFormProjectsEmployeeCapacity);
+};
+
+const removeFormInputAndNotification = () => {
+  const inputForms = document.querySelectorAll('.form-projects .form__item input');
+  inputForms.forEach(input => {
+    input.value = '';
+    if (input.classList.contains('incorrect-input')) input.classList.remove('incorrect-input');
+    if (input.classList.contains('correct-input')) input.classList.remove('correct-input');
+  });
+
+  const notificationsTags = document.querySelectorAll('.form-projects .form-notification');
+  notificationsTags.forEach(notification => {
+    notification.remove();
+  });
 };
--- src/script/requirementForms.js
+++ src/script/requirementForms.js
@@ -2,7 +2,7 @@ export const addRequirementForForm = (form, countChar) => {
   form.addEventListener('input', (e) => {
     const valueInput = e.target.value;
     const counter = valueInput.length;
-    const notificationElement = document.querySelector('.form-notification');
+    const notificationElement = form.nextElementSibling;
 
     let charCounter = 0;
     const arrValueInput = valueInput.split('');
@@ -13,17 +13,28 @@ export const addRequirementForForm = (form, countChar) => {
     });
 
     if (counter < countChar && !notificationElement) {
-      incorrectInput(form, `Project name must be at least ${countChar} characters`);
+      incorrectInput(form, `You must enter at least ${countChar} alphanumeric characters`);
     }
 
-    if (charCounter >= countChar && notificationElement) removeNotification(form);
+    if (charCounter >= countChar && notificationElement) {
+      removeNotification(form);
+      correctInput(form);
+    }
+
+    checkCorrectDataInInput();
   });
 };
 
 const incorrectInput = (input, notification) => {
   input.classList.add('incorrect-input');
+  if (input.classList.contains('correct-input')) input.classList.remove('correct-input');
   const elementNotification = createNotification(notification);
   input.after(elementNotification);
+};
+
+const correctInput = (input) => {
+  if (input.classList.contains('incorrect-input')) input.classList.remove('incorrect-input');
+  input.classList.add('correct-input');
 };
 
 const createNotification = (notification) => {
@@ -36,4 +47,67 @@ const createNotification = (notification) => {
 const removeNotification = (input) => {
   const notificationElement = input.nextElementSibling;
   notificationElement.remove();
+};
+
+export const addRequirementForFormBudget = (form) => {
+  form.addEventListener('input', (e) => {
+    const valueInput = e.target.value;
+    const notificationElement = form.nextElementSibling;
+
+    const arrValue = valueInput.split('.');
+
+    if (arrValue[1] && arrValue[1].length > 3) {
+      const valueInputNum = Number(valueInput);
+      form.value = valueInputNum.toFixed(2);
+    }
+
+    if (!valueInput || valueInput[0] === '0') {
+      if (!notificationElement) incorrectInput(form, 'You must enter the budget amount');
+    }
+
+    if (valueInput && notificationElement) {
+      removeNotification(form);
+      correctInput(form);
+    }
+
+    checkCorrectDataInInput();
+  });
+};
+
+export const addRequirementForFormEmployeeCapacity = (form) => {
+  form.addEventListener('input', (e) => {
+    const valueInput = e.target.value;
+    const notificationElement = form.nextElementSibling;
+    const arrValue = valueInput.split('.');
+
+    if (!valueInput || arrValue[1] || valueInput[0] === '0') {
+      if (!notificationElement) incorrectInput(form, 'You must enter integer, minimum 1');
+    }
+
+    if (valueInput && notificationElement) {
+      removeNotification(form);
+      correctInput(form);
+    }
+
+    checkCorrectDataInInput();
+  });
+};
+
+const checkCorrectDataInInput = () => {
+  const allInputInForms = document.querySelectorAll('.form-projects .form__item');
+  const countAllInputInForms = allInputInForms.length;
+
+  const allCorrectInput = document.querySelectorAll('.form-projects .correct-input');
+  const countAllCorrectInput = allCorrectInput.length;
+
+  const btnAddForm = document.querySelector('.form-btn-add');
+  const hasDisabledBtn = btnAddForm.hasAttribute('disabled');
+
+  if (countAllInputInForms === countAllCorrectInput && hasDisabledBtn) {
+    btnAddForm.removeAttribute('disabled');
+  }
+
+  if (countAllInputInForms !== countAllCorrectInput && !hasDisabledBtn) {
+    btnAddForm.setAttribute('disabled', '');
+  }
 };
