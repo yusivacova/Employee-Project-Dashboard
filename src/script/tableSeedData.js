857688bd5eb1b688f30dbf2a201bd8da9055c89e
yusivacova <sivacovayulia@gmail.com>
2026-04-30 11:59:57 +0300

    feat: add the ability to copy data from seed table

--- src/script/tableSeedData.js
+++ src/script/tableSeedData.js
@@ -1,4 +1,5 @@
-import { MONTHLY_DATA } from './dataApp';
+import { MONTHLY_DATA, defineDateKey } from './dataApp';
+import { updateTablesProjectsAndEmployees, removeTablesProjectsAndEmployees } from './localStorage';
 
 export function createTableSeedData() {
   const bodyTable = document.querySelector('.seed-data .table__body');
@@ -56,5 +57,38 @@ const createButtonSeed = (container) => {
   btnSeed.textContent = 'Seed';
   newTd.append(btnSeed);
 
+  btnSeed.addEventListener('click', (e) => {
+    const clickTag = e.target;
+    seedEmployeeAndProject(clickTag);
+    const seedData = document.querySelector('.seed-data');
+    seedData.classList.add('inactive-class');
+  });
+
   return btnSeed;
+};
+
+const seedEmployeeAndProject = (clickBtn) => {
+  const parent = clickBtn.closest('.table__item');
+  const keySelectetedObjData = parent.getAttribute('data-index');
+  const keyObjDataCurrent = defineDateKey();
+
+  if (!MONTHLY_DATA[keyObjDataCurrent]) {
+    MONTHLY_DATA[keyObjDataCurrent] = { employees: [], projects: [], total: '$0.00' };
+  }
+
+  MONTHLY_DATA[keySelectetedObjData].projects.forEach((project) => {
+    MONTHLY_DATA[keyObjDataCurrent].projects.push(project);
+  });
+
+  MONTHLY_DATA[keySelectetedObjData].employees.forEach((employee) => {
+    MONTHLY_DATA[keyObjDataCurrent].employees.push(employee);
+  });
+
+  removeTablesProjectsAndEmployees();
+  updateTablesProjectsAndEmployees();
+
+  console.log(MONTHLY_DATA);
+
+  console.log(clickBtn, 'clickBtn');
+  console.log('parent', parent);
 };
